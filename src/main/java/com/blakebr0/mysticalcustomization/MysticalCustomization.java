c976ece2f0360c2d2994c46d103779e941242d43
BlakeBr0 <luasmrks@gmail.com>
2020-04-04 00:07:12 -0500

    Initial mod setup

---
+++ src/main/java/com/blakebr0/mysticalcustomization/MysticalCustomization.java
@@ -0,0 +1,30 @@
+package com.blakebr0.mysticalcustomization;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(MysticalCustomization.MOD_ID)
+public class MysticalCustomization {
+    public static final String MOD_ID = "mysticalcustomization";
+    public static final String NAME = "Mystical Customization";
+
+    public MysticalCustomization() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        bus.register(this);
+    }
+
+    @SubscribeEvent
+    public void onCommonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
